11d7187623115be8b8ab029175659251fbc3d8e0
beatriz-raposo <162373882+beatriz-raposo@users.noreply.github.com>
2024-06-21 17:58:53 -0300

    Feat: Criando estrutura do login.

---
+++ src/Pages/Login/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export function Login() {
+
+  
+  return (
+    
+      <View>
+            <Text>Entrar no Telegram</Text>
+      </View>
+
+  )
+}
