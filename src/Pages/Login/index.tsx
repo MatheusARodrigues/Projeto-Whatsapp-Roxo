ea54cbca91c899ca31cebeb818c7972424201416
beatriz-raposo <162373882+beatriz-raposo@users.noreply.github.com>
2024-06-23 11:11:58 -0300

    Feat: Criando e estilizando login.

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,14 +1,61 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Text, View, Image, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { TextInputComponent } from '../../Components/TextInput'
+import CustomButton from '../../Components/Botao/CustomButton';
+import { useNavigation } from '@react-navigation/native';
+import Logo from '../../Assets/branca-whatsapp-150.png'
+import { styles } from './style';
 
-export function Login() {
+
+export function Login() { 
+
+  const [number, setNumber] = useState<string>();
+  const [password, setPassword] = useState<string>('');
+  
+  const navigator = useNavigation();
+ 
+  const handleNumber = (value: string) => {
+    setNumber(value);
+  }
+
+  const handlePassword = (value: string) => {
+    setPassword(value)
+  }
+
 
   
   return (
     
-      <View>
-            <Text>Entrar no Telegram</Text>
-      </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
 
-  )
+    <View style={styles.container}>
+        <Image style={styles.logo} source={Logo} alt="Logo tela login"/>
+
+        <Text style={styles.title}>Bem vindo(a) ao WhatsApp</Text>
+
+      <TextInputComponent
+        recebendoFuncao={handleNumber}
+        recebendoValue={number}
+        recebendoPlaceHolder="Digite seu número"
+        
+      />
+
+      <TextInputComponent
+        recebendoFuncao={handlePassword}
+        recebendoValue={password}
+        recebendoPlaceHolder="Digite sua senha"
+        recebendoTipoDoInput={true}
+      />
+
+      <CustomButton 
+      buttonStyle={styles.button} 
+      textStyle={{ color: '#4a148c', fontSize: 20, fontWeight: 'bold' }} 
+      title="Entrar" 
+      handleOnChange={handleLogin}
+      />
+
+    </View>
+  </TouchableWithoutFeedback>
+)
+
 }
--- src/Pages/Login/style.ts
+++ src/Pages/Login/style.ts
@@ -1,4 +1,38 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#4a148c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  logo: {
+    width: 160,
+    height: 160,
+  //   margin: 40,
+  },
+
+  title: {
+    fontSize: 35,
+    color: 'white',
+    textAlign: 'center',
+    margin: 20,
+    fontWeight: 'bold',
+  },
+
+  button: {
+    backgroundColor: '#ede7f6',
+    paddingVertical: 20,
+    paddingHorizontal: 90,
+    borderRadius: 10,
+    width: '90%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+  
+  }
+
+  
 })
