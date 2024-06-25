b1cd09366012d252d7f157dbceda88a303cbdfed
Nicks-Souza <nicolas.souza990@gmail.com>
2024-06-25 00:11:23 -0300

    Fix: resolvendo bug nos botões de arquivar e desarquivar

--- src/Routes/StackRoutes.tsx
+++ src/Routes/StackRoutes.tsx
@@ -6,8 +6,10 @@ import { DrawerTabsRoutes } from './DrawerTabsRoutes';
 import Configuracoes from "../Pages/Configuracoes";
 import TelaConversa from '../Pages/TelaConversa';
 import Arquivadas from '../Pages/Arquivadas';
+import {Perfil} from '../Pages/Perfil';
 import { AuthProvider  } from '../Hooks/useAuth';
 import { ThemeProvider } from '../Components/Tema/themeContext';
+import { MessagesProvider } from '../Components/Messages/MessageContext';
 
 export type ParametrosDaRota = {
     StackLogin: { name: string };
@@ -15,16 +17,19 @@ export type ParametrosDaRota = {
     StackConfiguracoes: { name: string };
     StackTelaConversas: { name: string };
     Arquivadas: {name: string}; 
+    StackPerfil: { name: string; phone: string; description: string };
 };
 
 const Stack = createNativeStackNavigator<ParametrosDaRota>();
 
 export function StackRoutes() {
     return (
+        <MessagesProvider>
         <ThemeProvider>
             <NavigationContainer>
             <AuthProvider>
             <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='StackLogin'>
+                <Stack.Screen name="StackPerfil" component={Perfil} />
                 <Stack.Screen name= "StackLogin" component={Login} />
                 <Stack.Screen name= "StackConfiguracoes" component={Configuracoes} />
                 <Stack.Screen name="StackTelaConversas" component={TelaConversa} />
@@ -34,5 +39,6 @@ export function StackRoutes() {
             </AuthProvider>
         </NavigationContainer>
         </ThemeProvider>
+        </MessagesProvider>
     );
 };
